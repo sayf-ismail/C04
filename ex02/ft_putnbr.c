90ca824425283fcc4e6ab7bbff57de778b5d467d
Sayf Tun-ismail <stun-ism@u90z02s03.42kl.edu.my>
2021-03-10 18:23:43 +0800

    completed ex02 & ex03

---
+++ ex02/ft_putnbr.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stun-ism <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/09 12:07:34 by stun-ism          #+#    #+#             */
+/*   Updated: 2021/03/09 16:29:28 by stun-ism         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print_min(void)
+{
+	ft_putchar('-');
+	ft_putchar('2');
+	ft_putchar('1');
+	ft_putchar('4');
+	ft_putchar('7');
+	ft_putchar('4');
+	ft_putchar('8');
+	ft_putchar('3');
+	ft_putchar('6');
+	ft_putchar('4');
+	ft_putchar('8');
+}
+
+void	ft_putnbr(int nb)
+{
+	char	c;
+
+	if (nb == -2147483648)
+	{
+		print_min();
+		return ;
+	}
+	if (nb < 0)
+	{
+		c = '-';
+		ft_putchar(c);
+		nb *= -1;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar('0' + nb);
+}
